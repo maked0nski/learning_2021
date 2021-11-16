321fae69269d8478d027060e1ca42cf4179ca411
maked0nski <maked0nskihtc@gmail.com>
2021-11-16 22:30:58 +0200

    js_home_work_04 home, class, additional

--- js/lesson_4/classwork/main.js
+++ js/lesson_4/classwork/main.js
@@ -14,64 +14,158 @@
 
 function fuMin(a, b, c) {
     let tmp = a;
-    tmp =(b < tmp) ? b : tmp
-    console.log ((c < tmp) ? c : tmp)
+    tmp = (b < tmp) ? b : tmp
+    console.log((c < tmp) ? c : tmp)
 }
+
 let arrtest = [
-    [1,2,3],
-    [1,3,2],
-    [2,1,3],
-    [2,3,1],
-    [3,2,1],
-    [3,1,2]
+    [1, 2, 3],
+    [1, 3, 2],
+    [2, 1, 3],
+    [2, 3, 1],
+    [3, 2, 1],
+    [3, 1, 2]
 ]
 for (let ell of arrtest) {
-    fuMin(ell[0],ell[1],ell[2])
+    fuMin(ell[0], ell[1], ell[2])
 }
 
 // - створити функцію яка приймає три числа та виводить найбільше. (Без Math.max!)
 
 function fuMax(a, b, c) {
     let tmp = a;
-    tmp =(b < tmp) ? tmp : b
-    console.log ((c < tmp) ? tmp : c)
+    tmp = (b < tmp) ? tmp : b
+    console.log((c < tmp) ? tmp : c)
 }
+
 for (let ell of arrtest) {
-    fuMax(ell[0],ell[1],ell[2])
+    fuMax(ell[0], ell[1], ell[2])
 }
 
 // - створити функцію яка повертає найбільше число з масиву
-function RandomMinMax(a = 1, b = 10) {
-    return Math.floor(Math.random() * (b - a)) + a;
-}
-let arr=[]
-let k;
-while (arr.length < 90) {
-    arr.push(RandomMinMax(1,500));
-    k++
-}
+
+
+let arr = CreateNumArr(90, 0, 500)
+
 
 console.log(arr);
 
-// function fuMaxArr (arr){
-//     let arrMax = arr[0];
-//     for (let i = 0; i < arr.length-1; i++) {
-//         arrMax= (arr[i]<=arrMax) ? arrMax: arr[i]
-//     }
-//     return arrMax
-// }
+function fuMaxArr(arr) {
+    let arrMax = arr[0];
+    for (let i = 0; i < arr.length - 1; i++) {
+        arrMax = (arr[i] <= arrMax) ? arrMax : arr[i]
+    }
+    return arrMax
+}
+
+// orr
+//
 // function fuMaxArr (arr){
 //       return Math.max.apply(null, arr)
 // }
 
-
 console.log(fuMaxArr(arr));
-// - створити функцію яка повертає найменьше число з масиву
 
-// - створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад [1,2,10]->13
+
+// - створити функцію яка повертає найменьше число з масиву
+function MinEllArr(arr) {
+    let min = arr[0];
+    for (let arrElement of arr) {
+        min = min >= arrElement ? arrElement : min;
+    }
+    return min
+}
+
+MinEllArr(arr)
+
+
+// - створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його.
+// Приклад [1,2,10]->13
+function SummEllArr(arr) {
+    let SummEll = 0;
+    for (let arrElement of arr) {
+        if (typeof arrElement === 'number') {
+            SummEll += arrElement
+        } else {
+            console.log('В масиве не только цифры')
+            break;
+        }
+    }
+    return SummEll
+}
+
+console.log(SummEllArr(arr));
+
 // - створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
-// - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше (Math використовувати заборонено);
+function SerArifmEllArr(arr) {
+    let SummEll = 0;
+    for (let arrElement of arr) {
+        if (typeof arrElement === 'number') {
+            SummEll += arrElement
+        } else {
+            console.log('В масиве не только цифры')
+            break;
+        }
+    }
+    return SummEll / (arr.length)
+}
+
+console.log(SerArifmEllArr(arr));
+// - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше 
+// (Math використовувати заборонено);
+
+function MinMaxEllArr(arr) {
+    let mixEll = arr[0];
+    let maxEll = arr[0];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === 'number') {
+            maxEll = (maxEll <= arr[i]) ? arr[i] : maxEll;
+            mixEll = (mixEll > arr[i]) ? arr[i] : mixEll;
+        } else {
+            console.log('В масиве не только цифры')
+            break;
+        }
+    }
+    console.log(`Найбільше число в масиві ${maxEll}`);
+    return mixEll
+}
+
+MinMaxEllArr(arr);
+
 // - створити функцію яка заповнює масив рандомними числами
 // (цей код генерує рандомні числа в діапазоні від 0 до 100 - Math.round(Math.random()*100)) та виводить його.
+
+
+
 // - створити функцію яка заповнює масив рандомними числами в діапазоні від 0 до limit. limit - аргумент, який характеризує кінцеве значення діапазону.
+
+function RandomMinMax(randMin = 1, randMax = 10) {
+    return Math.floor(Math.random() * (randMax - randMin)) + randMin;
+}
+
+function CreateNumArr(lengthArr, randMin, randMax) {
+    let arr = []
+    let k = 0;
+    while (arr.length < lengthArr) {
+        arr.push(RandomMinMax(randMin, randMax));
+        k++
+    }
+    return arr
+}
+
+CreateNumArr(15, 1,500)
+
+
+
 // - Функція приймає масив та робить з нього новий масив в зворотньому порядку. [1,2,3] -> [3, 2, 1].
+
+function ReversArr(arr) {
+    let newArr=[]
+    for (let i = arr.length-1; i >= 0; i--) {
+        newArr.push(arr[i])
+    }
+    return newArr
+}
+
+console.log(ReversArr(arr));
