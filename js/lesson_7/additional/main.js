c5d821f2e8cf88bad1b4b8e2a7587ae671059160
maked0nski <maked0nskihtc@gmail.com>
2021-11-29 00:01:16 +0200

    HW + CW + first additional № 7

--- js/lesson_7/additional/main.js
+++ js/lesson_7/additional/main.js
@@ -1,35 +1,120 @@
-//
 // - Створити клас або функцію конструктор, за допомоги якої можна створювати об'єкти наступного вигляду.
 // Конструктор повинен приймати значення для кожної властивості, в т.ч і для властивостей внутрішніх об'єктів
 //
+
 // {
-//     id: 1,
-//         name: 'Leanne Graham',
-//     username: 'Bret',
-//     email: 'Sincere@april.biz',
+//     id: 1;
+//     name: 'Leanne Graham';
+//     username: 'Bret';
+//     email: 'Sincere@april.biz';
 //     address: {
-//     street: 'Kulas Light',
+//         street: 'Kulas Light',
 //         suite: 'Apt. 556',
 //         city: 'Gwenborough',
 //         zipcode: '92998-3874',
 //         geo: {
-//         lat: '-37.3159',
+//             lat: '-37.3159',
 //             lng: '81.1496'
-//     }
-// },
-//     phone: '1-770-736-8031 x56442',
-//         website: 'hildegard.org',
+//         }
+//     };
+//     phone: '1-770-736-8031 x56442';
+//     website: 'hildegard.org';
 //     company: {
-//     name: 'Romaguera-Crona',
-//         catchPhrase: 'Multi-layered client-server neural-net',
+//         name: 'Romaguera-Crona';
+//         catchPhrase: 'Multi-layered client-server neural-net';
 //         bs: 'harness real-time e-markets'
+//     }
 // }
+
+
+
+// // підкажіть чи можна таким способом виконати це завдання, але в мене не вийшло додати при створенні let person5 =  new Person( 1, 'Leanne Graham', 'Bret', 'Sincere@april.biz', adress ???, ) та решта вкладені
+// class Geo {
+//     constructor(lat, lng) {
+//         this.lat = lat;
+//         this.lng = lng;
+//     }
 // }
-//
+// class Address {
+//     constructor(street, suite, city, zipcode) {
+//         this.street = street;
+//         this.suite = suite;
+//         this.city = city;
+//         this.zipcode = zipcode;
+//         this.geo = new Geo();
+//     }
+// }
+// class Company {
+//     constructor(name, catchPhrase, bs) {
+//         this.name = name;
+//         this.catchPhrase = catchPhrase;
+//         this.bs = bs;
+//     }
+// }
+// class Person {
+//     constructor(id, name, username, email, address, phone, website, company) {
+//         this.id = id;
+//         this.name = name;
+//         this.username = username;
+//         this.email = email;
+//         this.address = new Address();
+//         this.phone = phone;
+//         this.website = website;
+//         this.company = new Company();
+//     }
+// }
+
+
+
+
+class Person {
+    constructor(id, name, username, email, street, suite, city, zipcode, lat, lng, phone, website, companyName, companyCatchPhrase, companyBs) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.address = {
+            street: street,
+            suite: suite,
+            city: city,
+            zipcode: zipcode,
+            geo: {lat: lat, lng: lng}
+        };
+        this.phone = phone;
+        this.website = website;
+        this.company = {
+            name: companyName,
+            catchPhrase: companyCatchPhrase,
+            bs: companyBs
+        };
+    }
+}
+
+let person1 = new Person(
+    1,
+    'Leanne Graham',
+    'Bret',
+    'Sincere@april.biz',
+    'Kulas Light',
+    'Apt. 556',
+    'Gwenborough',
+    '92998-3874',
+    '-37.3159',
+    '81.1496',
+    '1-770-736-8031 x56442',
+    'hildegard.org',
+    'Romaguera-Crona',
+    'Multi-layered client-server neural-net',
+    'harness real-time e-markets'
+)
+
+console.log(person1)
+
+
 //
 // -  Створити функцію конструктор / клас  який описує об'єкт тегу
 // Поля :
-//     -назва тегу ()
+// - назва тегу ()
 // - опис його дій
 // - масив з атрибутами (2-3 атрибути максимум)
 // Кожен атрибут описати як окремий який буде містити
@@ -37,6 +122,7 @@
 // -опис дії атрибуту
 // інформацію брати з htmlbook.ru
 //
+
 // Таким чином описати теги
 // -a
 // -div
@@ -56,5 +142,9 @@
 //     {/*...*/},
 //     {/*...*/},
 // ]
+// }
+
+// class TagDescription {
+//     titleOfTag; action; attrs
 //
 // }
