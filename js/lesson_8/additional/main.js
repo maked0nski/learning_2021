7db14edc078d872cd05958ca1e65b381a7db2599
maked0nski <maked0nskihtc@gmail.com>
2021-11-30 23:31:12 +0200

    HW + CW + Add № 8

--- js/lesson_8/additional/main.js
+++ js/lesson_8/additional/main.js
@@ -1,2 +1,20 @@
 // написати рекурсивну функцію, яка збирає всі назви класів з файлу rules.html в окремий масив.
 // масив вивести в консоль
+
+let classArr = []
+function allEllementRec(tag) {
+    if (tag.children.length) {
+        for (const element of tag.children) {
+            allEllementRec(element);
+        }
+    }
+    if(tag.classList.length){
+        for (const tagElement of tag.classList) {
+            classArr.push(tagElement)
+        }
+    }
+}
+allEllementRec(document.body);
+
+
+console.log(classArr)
