5ec1d61cfa6fd612f64a79e8356cdb2c145c66ac
maked0nski <maked0nskihtc@gmail.com>
2021-12-02 04:24:23 +0200

    Домашня, класна, додаткова + наброски другої додаткової № 9

--- js/lesson_9/super_additional/main.js
+++ js/lesson_9/super_additional/main.js
@@ -1,65 +1,98 @@
+// - За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement,
+// розділивши всі властивості по своїм блокам (div>div*4)
+// - За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement,
+// розділивши всі властивості по своїм блокам , блок з адресою зробити окремим блоком, з блоками для кожної властивості
+//
+let users = [{
+    name: 'vasya',
+    age: 31,
+    status: false,
+    address: {city: 'Lviv', country: 'Ukraine', street: 'Shevchenko', houseNumber: 1}
+}, {
+    name: 'petya',
+    age: 30,
+    status: true,
+    address: {city: 'New York', country: 'USA', street: 'East str', houseNumber: 21}
+}, {
+    name: 'kolya',
+    age: 29,
+    status: true,
+    address: {city: 'Budapest', country: 'Hungary', street: 'Kuraku', houseNumber: 78}
+}, {
+    name: 'olya',
+    age: 28,
+    status: false,
+    address: {city: 'Prague', country: 'Czech', street: 'Paster', houseNumber: 56}
+}, {
+    name: 'max',
+    age: 30,
+    status: true,
+    address: {city: 'Istanbul', country: 'Turkey', street: 'Mikar', houseNumber: 39}
+}, {
+    name: 'anya',
+    age: 31,
+    status: false,
+    address: {city: 'Rio', country: 'Brasil', street: 'Ronaldi', houseNumber: 5}
+}, {
+    name: 'oleg',
+    age: 28,
+    status: false,
+    address: {city: 'Montreal', country: 'Canada', street: 'Acusto', houseNumber: 90}
+}, {
+    name: 'andrey',
+    age: 29,
+    status: true,
+    address: {city: 'Quebeck', country: 'Canada', street: 'Binaro', houseNumber: 33}
+}, {
+    name: 'masha',
+    age: 30,
+    status: true,
+    address: {city: 'Moscow', country: 'Russia', street: 'Gogolia', houseNumber: 1}
+}, {
+    name: 'olya',
+    age: 31,
+    status: false,
+    address: {city: 'Portland', country: 'USA', street: 'Forest str', houseNumber: 4}
+}, {
+    name: 'max',
+    age: 31,
+    status: true,
+    address: {city: 'Cairo', country: 'Egypt', street: 'Seashore', houseNumber: 45}
+}];
+//
 // - З масиву users за допомогою циклу витягнути адреси користувачів і записати (скопіювати) їх в інший порожній масив.
-// - За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement. Всі данні в одному блоці.
-// - За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement, розділивши всі властивості по своїм блокам (div>div*4)
-// - За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement, розділивши всі властивості по своїм блокам , блок з адресою зробити окремим блоком, з блоками для кожної властивості
-//
-// let users = [{
-//     name: 'vasya',
-//     age: 31,
-//     status: false,
-//     address: {city: 'Lviv', country: 'Ukraine', street: 'Shevchenko', houseNumber: 1}
-// }, {
-//     name: 'petya',
-//     age: 30,
-//     status: true,
-//     address: {city: 'New York', country: 'USA', street: 'East str', houseNumber: 21}
-// }, {
-//     name: 'kolya',
-//     age: 29,
-//     status: true,
-//     address: {city: 'Budapest', country: 'Hungary', street: 'Kuraku', houseNumber: 78}
-// }, {
-//     name: 'olya',
-//     age: 28,
-//     status: false,
-//     address: {city: 'Prague', country: 'Czech', street: 'Paster', houseNumber: 56}
-// }, {
-//     name: 'max',
-//     age: 30,
-//     status: true,
-//     address: {city: 'Istanbul', country: 'Turkey', street: 'Mikar', houseNumber: 39}
-// }, {
-//     name: 'anya',
-//     age: 31,
-//     status: false,
-//     address: {city: 'Rio', country: 'Brasil', street: 'Ronaldi', houseNumber: 5}
-// }, {
-//     name: 'oleg',
-//     age: 28,
-//     status: false,
-//     address: {city: 'Montreal', country: 'Canada', street: 'Acusto', houseNumber: 90}
-// }, {
-//     name: 'andrey',
-//     age: 29,
-//     status: true,
-//     address: {city: 'Quebeck', country: 'Canada', street: 'Binaro', houseNumber: 33}
-// }, {
-//     name: 'masha',
-//     age: 30,
-//     status: true,
-//     address: {city: 'Moscow', country: 'Russia', street: 'Gogolia', houseNumber: 1}
-// }, {
-//     name: 'olya',
-//     age: 31,
-//     status: false,
-//     address: {city: 'Portland', country: 'USA', street: 'Forest str', houseNumber: 4}
-// }, {
-//     name: 'max',
-//     age: 31,
-//     status: true,
-//     address: {city: 'Cairo', country: 'Egypt', street: 'Seashore', houseNumber: 45}
-// }];
-//
+let adressArr = []
+for (const user of users) {
+    adressArr.push(user.address)
+}
+// console.log(adressArr)
+
+
+// - За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement.
+// Всі данні в одному блоці.
+let divForThisTask = document.createElement('div');
+
+
+function someFunction(arr) {
+    let blockDiv = document.createElement('div');
+    for (const user in arr) {
+        if (typeof arr[user]==="object"){
+            someFunction(user)
+        }
+        blockDiv.innerText += `${user} - ${arr[user]} /n`
+    }
+    divForThisTask.append(blockDiv)
+    return divForThisTask
+}
+
+for (const user of users) {
+    divForThisTask.append(someFunction(user))
+}
+
+document.body.append(divForThisTask)
+
+
+
 //
 //
 //
