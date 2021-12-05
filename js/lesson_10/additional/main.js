a2521867440aacf2a01f8b973e8acbf54b04bdc2
maked0nski <maked0nskihtc@gmail.com>
2021-12-05 17:21:29 +0200

    hw + cw № 10

--- js/lesson_10/additional/main.js
+++ js/lesson_10/additional/main.js
@@ -2,30 +2,72 @@
 //     При лівому кліку миші вивести в консоль інформацію про блок або елемент на який відбувся клік.
 //     Інформація яку потрібно вивести: Назва тегу, список класів, список ід, розміри в форматі висота*ширина
 //
+// let targetObject = document.body
+// targetObject.addEventListener("click", function (e) {
+//     console.log(this)
+// })
+
+// document.body.onclick = function (ev) {
+//     console.log(`Ви вибрали елемент з тегом ${ev.target.localName}, id: ${ev.target.id}, список класов: ${ev.target.classList.value}, Ширина*Висота блока : ${ev.target.offsetWidth}px * ${ev.target.offsetHeight}px`)
+// }
+
+
 // - Описати скріпт, котрий, якщо доєднати до будь-якої сторінки дозволить зробити наступне:
 //     При лівому кліку миші  зробить popup (спливаючий блок) в якому буде вся інформація про блок.
-//     Інформація яку потрібно вивести в popup: Назва тегу, список класів, список ід, розміри в форматі висота*ширина
+//     Інформація яку потрібно вивести в popup: Назва тегу, список класів, список ід, розміри в
+//     форматі висота*ширина
 //
+
+document.body.onclick = function (ev) {
+    let pop = document.createElement('div');
+    pop.id = 'popUp'
+    let popContent = document.createElement('div');
+    pop.append(popContent);
+    pop.style.width = '100%';
+    pop.style.minHeight = '100%';
+    pop.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    pop.style.overflow = 'hidden';
+    pop.style.position = 'fixed';
+    pop.style.top = '0px';
+    popContent.style.margin = '40px auto 0px auto';
+    popContent.style.width = '250px';
+    popContent.style.minHeight = '50px';
+    popContent.style.padding = '10px';
+    popContent.style.backgroundColor = '#c5c5c5';
+    popContent.style.borderRadius = '5px';
+    popContent.innerHTML = `Ви вибрали елемент з тегом ${ev.target.localName},</br> id: ${ev.target.id},</br> список класов: ${ev.target.classList.value},</br> Розміри блока : ${ev.target.offsetWidth}px * ${ev.target.offsetHeight}px`;
+    document.body.append(pop)
+  }
+
+
+
+
 // -- взять массив пользователей
-// let usersWithAddress = [
-//     {id:1,name: 'vasya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
-//     {id:2,name: 'petya', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 1}},
-//     {id:3,name: 'kolya', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 121}},
-//     {id:4,name: 'olya', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 90}},
-//     {id:5,name: 'max', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 115}},
-//     {id:6,name: 'anya', age: 31, status: false, address: {city: 'Kyiv', street: 'Shevchenko', number: 2}},
-//     {id:7,name: 'oleg', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 22}},
-//     {id:8,name: 'andrey', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 43}},
-//     {id:9,name: 'masha', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 12}},
-//     {id:10,name: 'olya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
-//     {id:11,name: 'max', age: 31, status: true, address: {city: 'Ternopil', street: 'Shevchenko', number: 121}}
-// ];
-// - Создать три чекбокса. Каждый из них активирует фильтр для вышеуказаного массива. Фильтры могут работать как вместе так и по отдельности.
+let usersWithAddress = [
+    {id:1,name: 'vasya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
+    {id:2,name: 'petya', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 1}},
+    {id:3,name: 'kolya', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 121}},
+    {id:4,name: 'olya', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 90}},
+    {id:5,name: 'max', age: 30, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 115}},
+    {id:6,name: 'anya', age: 31, status: false, address: {city: 'Kyiv', street: 'Shevchenko', number: 2}},
+    {id:7,name: 'oleg', age: 28, status: false, address: {city: 'Ternopil', street: 'Shevchenko', number: 22}},
+    {id:8,name: 'andrey', age: 29, status: true, address: {city: 'Lviv', street: 'Shevchenko', number: 43}},
+    {id:9,name: 'masha', age: 30, status: true, address: {city: 'Kyiv', street: 'Shevchenko', number: 12}},
+    {id:10,name: 'olya', age: 31, status: false, address: {city: 'Lviv', street: 'Shevchenko', number: 16}},
+    {id:11,name: 'max', age: 31, status: true, address: {city: 'Ternopil', street: 'Shevchenko', number: 121}}
+];
+// - Создать три чекбокса. Каждый из них активирует фильтр для вышеуказаного массива. Фильтры могут работать
+// как вместе так и по отдельности.
 // 1й - отфильтровывает пользователей со статусом false (осталяет со статусом false)
 // 2й - оставляет старше 29 лет включительно
 // 3й - оставляет тех у кого город киев
 // Данные выводить в документ
 //
+
+
+
+
+
 //
 //
 // *****(Прям овердоз с рекурсией) Создать функцию которая принимает какой-либо элемент DOM-структуры .Функция создает в боди 2 кнопки (назад/вперед)
--- js/lesson_10/classwork/main.js
+++ js/lesson_10/classwork/main.js
@@ -1,22 +1,84 @@
-// - створити 2 форми  по 2 інпути в кожній. ствоирити кнопку при кліку на яку считується та виводиться на консоль інформація з цих 2х форм.
-//     Кнопка повинна лежати за межами форм (Щоб ьуникнути  перезавантаження сторінки)
-// Доступ до інпутів через Forms API. Отже дайте формі та інпутам всі необхідні атрибути.
+// - створити 2 форми  по 2 інпути в кожній. ствоирити
+// кнопку при кліку на яку считується та виводиться на
+// консоль інформація з цих 2х форм.
+// Кнопка повинна лежати за межами форм (Щоб уникнути
+// перезавантаження сторінки)
+// Доступ до інпутів через Forms API. Отже дайте формі та
+// інпутам всі необхідні атрибути.
+
+function getFormData() {
+    for (const form of document.forms) {
+        for (const formElement of form.elements) {
+            console.log(formElement.value);
+        }
+    }
+}
+
+
 //
-//
-// - Створити 3 инпута та кнопку. Один визначає кількість рядків, другий - кількість ячеєк, третій вмиіст ячеєк.
-//     При натисканні кнопки, вся ця інформація зчитується і формується табличка, з відповідним вмістом.
+// - Створити 3 инпута та кнопку. Один визначає кількість
+// рядків, другий - кількість ячеєк, третій вмиіст ячеєк.
+//     При натисканні кнопки, вся ця інформація зчитується
+//     і формується табличка, з відповідним вмістом.
 // (Додатковачастина для завдання)
-//
-//
+
+function createTable() {
+    let tableDiv = document.createElement('table');
+    tableDiv.setAttribute("border", "1px");
+    tableDiv.id = 'table'
+    let rows = +document.forms.tables.elements.row.value
+    let columns = +document.forms.tables.elements.column.value
+    let text = document.forms.tables.elements.text.value;
+    for (let row = 0; row < rows; row++) {
+        let tr = document.createElement('tr');
+        for (let column = 0; column < columns; column++) {
+            let td = document.createElement('td');
+            td.innerText = text;
+            tr.append(td)
+        }
+        tableDiv.append(tr)
+    }
+    document.body.append(tableDiv)
+}
+
+
 // - Сворити масив не цензцрних слів.
 //     Сворити інпут текстового типу.
 //     Якщо людина вводить слово і воно міститься в масиві не цензурних слів
 // кинути алерт з попередженням.
 //     Перевірку робити при натисканні на кнопку
 //
+let mat = ['khv', 'ilhv', 'igij']
+
+function checkMat() {
+    let str = document.forms.checkMat.elements.stroka.value;
+    let arr = str.split(' ')
+    let divRezult = document.getElementById('divRezult')
+    document.body.append(divRezult)
+    for (const arrElement of arr) {
+        if (mat.indexOf(arrElement) !== -1) {
+            divRezult.innerText = 'Обнаружен матюк'
+            divRezult.style.color = 'red'
+        } else {
+            divRezult.innerText = 'Матюк не  обнаружен'
+            divRezult.style.color = 'green'
+        }
+    }
+}
+
+
 //
 // - Сворити масив не цензцрних слів.
 //     Сворити інпут текстового типу.
 //     Потрібно перевіряти чи не містить ціле речення в собі погані слова.
 //     Кинути алерт з попередженням у випадку якщо містить.
 //     Перевірку робити при натисканні на кнопку
+
+
+//
+
+
+
+
+
+
